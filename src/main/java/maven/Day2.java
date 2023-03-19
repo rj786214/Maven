9c529073a4fc2199394eaff86e92b10147d53154
Raj Patel <rj786214@gmail.com>
2023-03-18 20:56:04 -0400

    change

--- src/main/java/maven/Day2.java
+++ src/main/java/maven/Day2.java
@@ -1,14 +1,11 @@
 package maven;
 
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
-
-
 
 public class Day2 {
 	WebDriver driver;
